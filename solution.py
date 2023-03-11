67dc77462c4016c83f21a621fd72022936b23dbc
sleeaw <43294806+sleeaw@users.noreply.github.com>
2023-03-11 10:15:30 +0800

    solution

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas
-
+##########q1
 list_size=['X'*1000+'S', 'M', 'X'*1000+'L']
 #print('XXL' in 'XXXL')
 n=input()
 n=int(n)
-
+list_true=[]
 if n>=1 and n<=1000:
     o=input()
     print(o) 
@@ -16,12 +16,16 @@ if n>=1 and n<=1000:
         if numbers[i] in list_size[0] or numbers[i] in list_size[1] or numbers[i] in list_size[2] :
             #print(numbers[i])
             #continue
-            print('Yes')
+            list_true.append(True)
 
         else:
-            print('No')
+            list_true.append(False)
+            #print('No')
         
-
+if False in list_true:
+    print('No')
+else:
+    print('Yes')
 
 
 
